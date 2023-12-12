dcfa013757d94af9a48c3fa47f354de4f6f2066c
enfyswon <enfyswon07@gmail.com>
2023-12-12 20:55:35 +0900

    mysql setting1

--- src/Screens/ListView.js
+++ src/Screens/ListView.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { SafeAreaView, FlatList, Text } from "react-native";
 
 const express = require("express");
@@ -9,20 +11,48 @@ app.get("/", function(req, res) {
 app.listen(3000, () => console.log("3000포트에서 대기중"));
 
 function ListView() {
-  const posts = [
-    {
-      id: 1,
-      title: "첫번째 책.",
-      contents: "내용입니다.",
-      date: "2023-11-26",
-    },
-    {
-      id: 2,
-      title: "두번째 책.",
-      contents: "내용입니다.",
-      date: "2023-11-27",
-    },
-  ];
+  // const posts = [
+  //   {
+  //     id: 1,
+  //     title: "첫번째 책.",
+  //     contents: "내용입니다.",
+  //     date: "2023-11-26",
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "두번째 책.",
+  //     contents: "내용입니다.",
+  //     date: "2023-11-27",
+  //   },
+  // ];
+
+  const [inputData, setInputData] = useState([{
+    book_id: '',
+    book_date: '',
+    book_title: '',
+    book_author: '',
+    book_publisher: '',
+    book_star: '',
+    book_review: '',
+    book_thumbnail: ''
+  }])
+
+  const [lastIdx, setLastIdx] = useState(0);
+
+  useEffect(async() => {
+    try {
+      const res = await axios.get('/api/booklist');
+      const _inputData = await res.data.map((rowData) => {
+        setLastIdx(lastIdx + 1),
+        {
+          book_id: rowData.book_id,
+          book_title: rowDate
+        }
+      })
+    } catch(e) {
+      console.error(e.message);
+    }
+  })
   return (
     <SafeAreaView>
       <FlatList
